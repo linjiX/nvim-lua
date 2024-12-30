a3cdfe7028fa727a51dec80abac7c8483bf3379f
linji.xue <linji.xue@graviti.com>
2024-12-30 12:27:31 +0800

    feat: use gitsigns status in lualine diff for immediate display

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -6,6 +6,23 @@ return {
         vim.opt.showmode = false
         return {
             sections = {
+                lualine_b = {
+                    "branch",
+                    {
+                        "diff",
+                        source = function()
+                            local gitsigns = vim.b.gitsigns_status_dict
+                            if gitsigns then
+                                return {
+                                    added = gitsigns.added,
+                                    modified = gitsigns.changed,
+                                    removed = gitsigns.removed,
+                                }
+                            end
+                        end,
+                    },
+                    "diagnostics",
+                },
                 lualine_x = {
                     { "copilot", show_colors = true },
                     "encoding",
