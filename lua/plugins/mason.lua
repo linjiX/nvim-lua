aed0a26eb435f0a881576159ab327c8a96a52644
linji.xue <linji.xue@graviti.com>
2025-01-06 16:14:21 +0800

    feat: add "cmd" and "keys" for "mason.nvim"

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -1,5 +1,7 @@
 return {
     "williamboman/mason.nvim",
+    cmd = "Mason",
+    keys = { { "<leader>m", ":Mason<CR>", silent = true, desc = "Open Mason" } },
     opts = {
         ui = { border = "rounded" },
     },
