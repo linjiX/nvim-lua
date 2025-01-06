0385a0d4bff2c7d7e6b8e4f6b73b706e76aed6fd
linji.xue <linji.xue@graviti.com>
2025-01-06 16:16:37 +0800

    feat: add customized mason registry to support "pyupgrade"

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -3,6 +3,10 @@ return {
     cmd = "Mason",
     keys = { { "<leader>m", ":Mason<CR>", silent = true, desc = "Open Mason" } },
     opts = {
+        registries = {
+            "github:mason-org/mason-registry",
+            ("file:%s/mason-registry"):format(vim.fn.stdpath("config")),
+        },
         ui = { border = "rounded" },
     },
 }
