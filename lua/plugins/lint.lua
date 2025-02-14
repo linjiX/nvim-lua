d8d117290baebb44ac4b1c449b15392f7e50a3f9
linji.xue <linji.xue@graviti.com>
2025-02-15 00:16:38 +0800

    style(lint): remove the useless outer braces

--- lua/plugins/lint.lua
+++ lua/plugins/lint.lua
@@ -1,29 +1,27 @@
 return {
-    {
-        "mfussenegger/nvim-lint",
-        event = { "BufReadPre", "BufNewFile" },
-        config = function()
-            local lint = require("lint")
+    "mfussenegger/nvim-lint",
+    event = { "BufReadPre", "BufNewFile" },
+    config = function()
+        local lint = require("lint")
 
-            lint.linters_by_ft = {
-                lua = { "luacheck" },
-                python = { "mypy" },
-                html = { "htmllint" },
-                javascript = { "eslint_d" },
-                typescript = { "eslint_d" },
-                vue = { "eslint_d" },
-                gitcommit = { "gitlint" },
+        lint.linters_by_ft = {
+            lua = { "luacheck" },
+            python = { "mypy" },
+            html = { "htmllint" },
+            javascript = { "eslint_d" },
+            typescript = { "eslint_d" },
+            vue = { "eslint_d" },
+            gitcommit = { "gitlint" },
+        }
+
+        vim.api.nvim_create_autocmd(
+            { "BufWritePost", "BufReadPost", "InsertLeave", "TextChanged" },
+            {
+                group = vim.api.nvim_create_augroup("nvim-lint", { clear = true }),
+                callback = function()
+                    lint.try_lint()
+                end,
             }
-
-            vim.api.nvim_create_autocmd(
-                { "BufWritePost", "BufReadPost", "InsertLeave", "TextChanged" },
-                {
-                    group = vim.api.nvim_create_augroup("nvim-lint", { clear = true }),
-                    callback = function()
-                        lint.try_lint()
-                    end,
-                }
-            )
-        end,
-    },
+        )
+    end,
 }
