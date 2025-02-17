899ecb2f73fee924b86341f53c418e4853631e5f
linji.xue <linji.xue@graviti.com>
2025-02-17 19:37:46 +0800

    perf(lint): skip stdin=false linters in InsertLeave and TextChanged

--- lua/plugins/lint.lua
+++ lua/plugins/lint.lua
@@ -14,14 +14,30 @@ return {
             gitcommit = { "gitlint" },
         }
 
-        vim.api.nvim_create_autocmd(
-            { "BufWritePost", "BufReadPost", "InsertLeave", "TextChanged" },
-            {
-                group = vim.api.nvim_create_augroup("nvim-lint", { clear = true }),
-                callback = function()
-                    lint.try_lint()
-                end,
-            }
-        )
+        local MyLint = vim.api.nvim_create_augroup("nvim-lint", { clear = true })
+
+        vim.api.nvim_create_autocmd({ "BufWritePost", "BufReadPost" }, {
+            group = MyLint,
+            callback = function()
+                lint.try_lint()
+            end,
+        })
+        vim.api.nvim_create_autocmd({ "InsertLeave", "TextChanged" }, {
+            group = MyLint,
+            callback = function()
+                local linters = lint.linters_by_ft[vim.bo.filetype]
+                if not linters then
+                    return
+                end
+
+                local names = vim.tbl_filter(function(name)
+                    return lint.linters[name].stdin
+                end, linters)
+
+                if #names > 0 then
+                    lint.try_lint(names)
+                end
+            end,
+        })
     end,
 }
