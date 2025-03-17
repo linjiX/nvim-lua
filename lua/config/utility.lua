a8241fbf7b7b6fcd1f53992f569646213dded558
linji.xue <linji.xue@graviti.com>
2025-03-17 15:39:11 +0800

    refactor: implement "lazy_require" in "config/utility.lua"

---
+++ lua/config/utility.lua
@@ -0,0 +1,30 @@
+local METATABLE = {
+    __index = function(self, key)
+        table.insert(self.keys, key)
+        return self
+    end,
+    __call = function(self, ...)
+        local target = nil
+        local args = { ... }
+
+        local fn = function()
+            if target == nil then
+                target = require(self.name)
+                for _, key in ipairs(self.keys) do
+                    target = target[key]
+                end
+            end
+            target(unpack(args))
+        end
+
+        return fn
+    end,
+}
+
+return {
+    lazy_require = function(name)
+        local result = { name = name, keys = {} }
+        setmetatable(result, METATABLE)
+        return result
+    end,
+}
