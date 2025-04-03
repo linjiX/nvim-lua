b2166965e5d6f29859e1c2719453de05b58b83b8
linji.xue <linji.xue@graviti.com>
2025-04-04 02:06:42 +0800

    feat(colorizer): add plugin "nvim-colorizer"

---
+++ lua/plugins/colorizer.lua
@@ -0,0 +1,39 @@
+local R = require("config.utility").lazy_require
+
+local ft = { "vue", "javascript", "typescript", "html", "css" }
+
+return {
+    "norcalli/nvim-colorizer.lua",
+    ft = ft,
+    cmd = "Colorizer",
+    keys = {
+        {
+            "[rh",
+            R("colorizer").attach_to_buffer(0),
+        },
+        {
+            "]rh",
+            R("colorizer").detach_from_buffer(0),
+        },
+        {
+            "yrh",
+            function()
+                local colorizer = require("colorizer")
+                if colorizer.is_buffer_attached(0) then
+                    colorizer.detach_from_buffer(0)
+                else
+                    colorizer.attach_to_buffer(0)
+                end
+            end,
+        },
+        {
+            "yrH",
+            R("colorizer").reload_all_buffers(),
+        },
+    },
+    opts = function()
+        vim.opt.termguicolors = true
+
+        return ft
+    end,
+}
