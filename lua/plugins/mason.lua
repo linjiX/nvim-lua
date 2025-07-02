2d277ec67d49a7aab41310860f4f2d7d302e16ca
linji.xue <linji.xue@graviti.com>
2025-07-02 12:18:01 +0800

    feat(mason): set "upgrade_pip" to true and use "vim.cmd" for key mapping

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -1,11 +1,16 @@
 return {
     "mason-org/mason.nvim",
     cmd = "Mason",
-    keys = { { "<Leader>m", ":Mason<CR>", silent = true, desc = "Open Mason" } },
+    keys = {
+        { "<Leader>m", vim.cmd.Mason, desc = "Open Mason" },
+    },
     opts = {
         registries = {
             "github:mason-org/mason-registry",
             "lua:registry",
+        },
+        pip = {
+            upgrade_pip = true,
         },
         ui = { border = "rounded" },
     },
