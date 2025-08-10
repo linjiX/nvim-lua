6232a2224e4a02157a1038990b2b2ce2d64c705d
linji.xue <linji.xue@graviti.com>
2025-08-10 12:58:48 +0800

    refactor(trouble): use lua API instead of cmd in keys and add basic opts

--- lua/plugins/trouble.lua
+++ lua/plugins/trouble.lua
@@ -1,27 +1,37 @@
+local trouble = require("utility").lazy_require("trouble")
+
 return {
     "folke/trouble.nvim",
     cmd = "Trouble",
     keys = {
         {
             "<Leader>td",
-            ":Trouble diagnostics toggle filter.buf=0<CR>",
+            trouble.toggle({ mode = "diagnostics", filter = { buf = 0 } }),
             desc = "Buffer Diagnostics (Trouble)",
         },
         {
             "<Leader>tD",
-            ":Trouble diagnostics toggle<CR>",
+            trouble.toggle({ mode = "diagnostics", win = { relative = "editor" } }),
             desc = "Diagnostics (Trouble)",
         },
         {
             "<Leader>tl",
-            ":Trouble loclist toggle<CR>",
+            trouble.toggle({ mode = "loclist" }),
             desc = "Location List (Trouble)",
         },
         {
             "<Leader>tq",
-            ":Trouble qflist toggle<CR>",
+            trouble.toggle({ mode = "qflist" }),
             desc = "Quickfix List (Trouble)",
         },
     },
-    opts = {},
+    opts = {
+        open_no_results = false,
+        focus = true,
+        win = { relative = "win" },
+        keys = {
+            ["<C-s>"] = nil,
+            ["<C-x>"] = "jump_split",
+        },
+    },
 }
