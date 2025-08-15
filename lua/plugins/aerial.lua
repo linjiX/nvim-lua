4d1367d97a318061ef94a4f76addc6c6f2e333c3
linji.xue <linji.xue@graviti.com>
2025-08-15 10:56:38 +0800

    feat(aerial): link highlight "AerialLine" to "CursorLineNr"

--- lua/plugins/aerial.lua
+++ lua/plugins/aerial.lua
@@ -1,30 +1,34 @@
 return {
     "stevearc/aerial.nvim",
     cmd = { "AerialOpen", "AerialToggle" },
-    opts = {
-        layout = {
-            width = 32,
-        },
-        attach_mode = "global",
-        keymaps = {
-            ["<C-s>"] = false,
-            ["<C-x>"] = "actions.jump_split",
-            ["<C-j>"] = false,
-            ["<C-k>"] = false,
-        },
-        show_guides = true,
-        guides = {
-            mid_item = "├╴",
-            last_item = "└╴",
-            nested_top = "│ ",
-            whitespace = "  ",
-        },
-        lsp = {
-            diagnostics_trigger_update = true,
-            priority = {
-                copilot = 0,
-                vue_ls = 20,
+    opts = function()
+        vim.api.nvim_set_hl(0, "AerialLine", { link = "CursorLineNr" })
+
+        return {
+            layout = {
+                width = 32,
             },
-        },
-    },
+            attach_mode = "global",
+            keymaps = {
+                ["<C-s>"] = false,
+                ["<C-x>"] = "actions.jump_split",
+                ["<C-j>"] = false,
+                ["<C-k>"] = false,
+            },
+            show_guides = true,
+            guides = {
+                mid_item = "├╴",
+                last_item = "└╴",
+                nested_top = "│ ",
+                whitespace = "  ",
+            },
+            lsp = {
+                diagnostics_trigger_update = true,
+                priority = {
+                    copilot = 0,
+                    vue_ls = 20,
+                },
+            },
+        }
+    end,
 }
