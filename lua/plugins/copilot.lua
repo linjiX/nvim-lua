7595b42033237024834910d42fcec390828416ef
linji.xue <linji.xue@graviti.com>
2025-10-01 17:00:20 +0800

    refactor(copilot): move "copilot-lualine" into dependencies

--- lua/plugins/copilot.lua
+++ lua/plugins/copilot.lua
@@ -1,88 +1,86 @@
 return {
-    {
-        "zbirenbaum/copilot.lua",
-        cmd = "Copilot",
-        event = "InsertEnter",
-        keys = {
-            {
-                "<Tab>",
-                function()
-                    if not require("copilot.suggestion").is_visible() then
-                        vim.api.nvim_feedkeys(vim.keycode("<Tab>"), "n", false)
-                        return
-                    end
+    "zbirenbaum/copilot.lua",
+    dependencies = {
+        "AndreM222/copilot-lualine",
+    },
+    cmd = "Copilot",
+    event = "InsertEnter",
+    keys = {
+        {
+            "<Tab>",
+            function()
+                if not require("copilot.suggestion").is_visible() then
+                    vim.api.nvim_feedkeys(vim.keycode("<Tab>"), "n", false)
+                    return
+                end
 
-                    require("copilot.suggestion").accept()
-                end,
-                mode = "i",
-                desc = "Accept Copilot Suggestion",
-            },
-            {
-                "<C-e>",
-                function()
-                    if not require("copilot.suggestion").is_visible() then
-                        vim.api.nvim_feedkeys(vim.keycode("<C-e>"), "n", false)
-                        return
-                    end
-
-                    require("copilot.suggestion").dismiss()
-                end,
-                mode = "i",
-                desc = "Dismiss Copilot Suggestion",
-            },
-            {
-                "[rg",
-                function()
-                    require("copilot.command").enable()
-                end,
-                desc = "Enable Copilot",
-            },
-            {
-                "]rg",
-                function()
-                    require("copilot.command").disable()
-                end,
-                desc = "Disable Copilot",
-            },
-            {
-                "yrg",
-                function()
-                    require("copilot.command").toggle()
-                end,
-                desc = "Toggle Copilot",
-            },
+                require("copilot.suggestion").accept()
+            end,
+            mode = "i",
+            desc = "Accept Copilot Suggestion",
         },
-        opts = {
-            panel = {
-                keymap = {
-                    jump_prev = "<C-k>",
-                    jump_next = "<C-j>",
-                    accept = "<CR>",
-                    refresh = "r",
-                    open = "<C-l>",
-                },
-                layout = {
-                    position = "right",
-                },
-            },
-            suggestion = {
-                auto_trigger = true,
-                keymap = {
-                    accept = false,
-                    accept_word = false,
-                    accept_line = false,
-                    next = "<C-j>",
-                    prev = "<C-k>",
-                    dismiss = "<C-]>",
-                },
-            },
-            filetypes = {
-                gitcommit = true,
-            },
-            copilot_model = "claude-sonnet-4",
+        {
+            "<C-e>",
+            function()
+                if not require("copilot.suggestion").is_visible() then
+                    vim.api.nvim_feedkeys(vim.keycode("<C-e>"), "n", false)
+                    return
+                end
+
+                require("copilot.suggestion").dismiss()
+            end,
+            mode = "i",
+            desc = "Dismiss Copilot Suggestion",
+        },
+        {
+            "[rg",
+            function()
+                require("copilot.command").enable()
+            end,
+            desc = "Enable Copilot",
+        },
+        {
+            "]rg",
+            function()
+                require("copilot.command").disable()
+            end,
+            desc = "Disable Copilot",
+        },
+        {
+            "yrg",
+            function()
+                require("copilot.command").toggle()
+            end,
+            desc = "Toggle Copilot",
         },
     },
-    {
-        "AndreM222/copilot-lualine",
+    opts = {
+        panel = {
+            keymap = {
+                jump_prev = "<C-k>",
+                jump_next = "<C-j>",
+                accept = "<CR>",
+                refresh = "r",
+                open = "<C-l>",
+            },
+            layout = {
+                position = "right",
+            },
+        },
+        suggestion = {
+            auto_trigger = true,
+            keymap = {
+                accept = false,
+                accept_word = false,
+                accept_line = false,
+                next = "<C-j>",
+                prev = "<C-k>",
+                dismiss = "<C-]>",
+            },
+        },
+        filetypes = {
+            gitcommit = true,
+        },
+        copilot_model = "claude-sonnet-4",
     },
 }
