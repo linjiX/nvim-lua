78fc6caeaf74a2091babf2d3f7388cb3c05f5377
linji.xue <linji.xue@graviti.com>
2026-03-06 12:17:40 +0800

    refactor(render-markdown): move config from "avante" to a separate file

---
+++ lua/plugins/render-markdown.lua
@@ -0,0 +1,9 @@
+local filetypes = { "markdown", "Avante" }
+
+return {
+    "MeanderingProgrammer/render-markdown.nvim",
+    ft = filetypes,
+    opts = {
+        file_types = filetypes,
+    },
+}
