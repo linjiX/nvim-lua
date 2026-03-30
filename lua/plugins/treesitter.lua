137291bc6fa4a559df5ea78dd34620898b06242d
linji.xue <linji.xue@graviti.com>
2026-03-30 23:51:10 +0800

    feat(treesitter)!: upgrade "nvim-treesitter" to "main" branch
    The reddit notification:
    https://www.reddit.com/r/neovim/comments/1ppa4ag/nvimtreesitter_breaking_changes/
    
    Also:
    Parser "jsonc" is removed due to it is unsupported anymore.

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -2,51 +2,29 @@ return {
     "nvim-treesitter/nvim-treesitter",
     build = ":TSUpdate",
     config = function()
-        require("nvim-treesitter.configs").setup({
-            ensure_installed = {
-                "bash",
-                "css",
-                "diff",
-                "dockerfile",
-                "git_config",
-                "git_rebase",
-                "gitcommit",
-                "gitignore",
-                "html",
-                "javascript",
-                "json",
-                "jsonc",
-                "lua",
-                "markdown",
-                "markdown_inline",
-                "python",
-                "regex",
-                "scss",
-                "toml",
-                "typescript",
-                "vim",
-                "vue",
-                "yaml",
-            },
-
-            highlight = {
-                enable = true,
-                additional_vim_regex_highlighting = false,
-            },
-
-            incremental_selection = {
-                enable = false,
-                keymaps = {
-                    init_selection = "<CR>",
-                    node_incremental = "<CR>",
-                    node_decremental = "<BS>",
-                    scope_incremental = "<TAB>",
-                },
-            },
-
-            indent = {
-                enable = true,
-            },
+        require("nvim-treesitter").install({
+            "bash",
+            "css",
+            "diff",
+            "dockerfile",
+            "git_config",
+            "git_rebase",
+            "gitcommit",
+            "gitignore",
+            "html",
+            "javascript",
+            "json",
+            "lua",
+            "markdown",
+            "markdown_inline",
+            "python",
+            "regex",
+            "scss",
+            "toml",
+            "typescript",
+            "vim",
+            "vue",
+            "yaml",
         })
 
         vim.opt.foldmethod = "expr"
