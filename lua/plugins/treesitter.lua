d602949448774a8b4dbe701c8eee499a113752ad
linji.xue <linji.xue@graviti.com>
2026-03-31 23:48:02 +0800

    feat(treesitter): start treesitter in "FileType" autocmd

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,30 +1,47 @@
+local parsers = {
+    "bash",
+    "css",
+    "diff",
+    "dockerfile",
+    "git_config",
+    "git_rebase",
+    "gitcommit",
+    "gitignore",
+    "html",
+    "javascript",
+    "json",
+    "lua",
+    "markdown",
+    "markdown_inline",
+    "python",
+    "regex",
+    "scss",
+    "toml",
+    "typescript",
+    "vim",
+    "vue",
+    "yaml",
+}
+
 return {
     "nvim-treesitter/nvim-treesitter",
     build = ":TSUpdate",
     config = function()
-        require("nvim-treesitter").install({
-            "bash",
-            "css",
-            "diff",
-            "dockerfile",
-            "git_config",
-            "git_rebase",
-            "gitcommit",
-            "gitignore",
-            "html",
-            "javascript",
-            "json",
-            "lua",
-            "markdown",
-            "markdown_inline",
-            "python",
-            "regex",
-            "scss",
-            "toml",
-            "typescript",
-            "vim",
-            "vue",
-            "yaml",
+        require("nvim-treesitter").install(parsers)
+
+        local filetypes = vim.iter(parsers)
+            :map(function(parser)
+                return vim.treesitter.language.get_filetypes(parser)
+            end)
+            :flatten()
+            :totable()
+
+        vim.api.nvim_create_autocmd("FileType", {
+            group = vim.api.nvim_create_augroup("MyTreesitter", { clear = true }),
+            pattern = filetypes,
+            callback = function()
+                vim.treesitter.start()
+            end,
         })
 
         vim.opt.foldmethod = "expr"
