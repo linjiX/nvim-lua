08ef519e4f90056b1c5469ce2314ecf212965701
linji.xue <linji.xue@graviti.com>
2026-04-17 12:46:36 +0800

    feat(copilot): adjust the NES trigger autocmds by hijacking copilot-lsp
    The trigger autocmds are updated from { "TextChangedI", "TextChanged" } to
    { "InsertLeave", "TextChanged" }

--- lua/plugins/copilot.lua
+++ lua/plugins/copilot.lua
@@ -1,10 +1,43 @@
 local command = require("config.utility").lazy_require("copilot.command")
 
+-- This function is copied from "copilot-lsp" and update autocmds to limit NES on Normal mode
+---@param client vim.lsp.Client
+---@param augroup integer
+local function hijack_lsp_on_init(client, augroup)
+    local debounced_request = require("copilot-lsp.util").debounce(
+        require("copilot-lsp.nes").request_nes,
+        vim.g.copilot_nes_debounce or 500
+    )
+    vim.api.nvim_create_autocmd({ "InsertLeave", "TextChanged" }, {
+        callback = function()
+            debounced_request(client)
+        end,
+        group = augroup,
+    })
+
+    vim.api.nvim_create_autocmd("BufEnter", {
+        callback = function()
+            local td_params = vim.lsp.util.make_text_document_params()
+            client:notify("textDocument/didFocus", {
+                textDocument = {
+                    uri = td_params.uri,
+                },
+            })
+        end,
+        group = augroup,
+    })
+end
+
 return {
     "zbirenbaum/copilot.lua",
     dependencies = {
         "AndreM222/copilot-lualine",
-        "copilotlsp-nvim/copilot-lsp",
+        {
+            "copilotlsp-nvim/copilot-lsp",
+            opts = function()
+                require("copilot-lsp.nes").lsp_on_init = hijack_lsp_on_init
+            end,
+        },
     },
     cmd = "Copilot",
     event = "InsertEnter",
