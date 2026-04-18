fd206c80d580eb77863ed302587019f30b9081e2
linji.xue <linji.xue@graviti.com>
2026-04-18 16:32:53 +0800

    feat(copilot): disable NES feature and remove dependency "copilot-lsp"

--- lua/plugins/copilot.lua
+++ lua/plugins/copilot.lua
@@ -1,44 +1,8 @@
 local command = require("config.utility").lazy_require("copilot.command")
 
--- This function is copied from "copilot-lsp" and update autocmds to limit NES on Normal mode
----@param client vim.lsp.Client
----@param augroup integer
-local function hijack_lsp_on_init(client, augroup)
-    local debounced_request = require("copilot-lsp.util").debounce(
-        require("copilot-lsp.nes").request_nes,
-        vim.g.copilot_nes_debounce or 500
-    )
-    vim.api.nvim_create_autocmd({ "InsertLeave", "TextChanged" }, {
-        callback = function()
-            debounced_request(client)
-        end,
-        group = augroup,
-    })
-
-    vim.api.nvim_create_autocmd("BufEnter", {
-        callback = function()
-            local td_params = vim.lsp.util.make_text_document_params()
-            client:notify("textDocument/didFocus", {
-                textDocument = {
-                    uri = td_params.uri,
-                },
-            })
-        end,
-        group = augroup,
-    })
-end
-
 return {
     "zbirenbaum/copilot.lua",
-    dependencies = {
-        "AndreM222/copilot-lualine",
-        {
-            "copilotlsp-nvim/copilot-lsp",
-            opts = function()
-                require("copilot-lsp.nes").lsp_on_init = hijack_lsp_on_init
-            end,
-        },
-    },
+    dependencies = { "AndreM222/copilot-lualine" },
     cmd = "Copilot",
     event = "InsertEnter",
     keys = {
@@ -81,15 +45,6 @@ return {
                 accept_line = false,
                 next = "<C-j>",
                 prev = "<C-k>",
-                dismiss = "<C-e>",
-            },
-        },
-        nes = {
-            enabled = true,
-            auto_trigger = true,
-            keymap = {
-                accept_and_goto = "<Tab>",
-                accept = "<S-Tab>",
                 dismiss = "<C-e>",
             },
         },
