5ac3b69e477b349165abd34f2f12be37602b0544
linji.xue <linji.xue@graviti.com>
2026-04-23 13:50:35 +0800

    feat(lspconfig): add lspconfig "lua_ls_nvim" for neovim config files

--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -2,6 +2,43 @@ local function get_vue_plugin_path()
     return vim.fn.expand("$MASON/packages/vue-language-server/node_modules/@vue/language-server")
 end
 
+local function is_nvim_config_buffer(bufnr)
+    local path = vim.api.nvim_buf_get_name(bufnr)
+    if not path then
+        return false
+    end
+
+    return vim.fs.relpath(vim.fn.stdpath("config"), path) ~= nil
+end
+
+local function lua_root_dir_getter(root_markers)
+    return function(bufnr, on_dir)
+        if is_nvim_config_buffer(bufnr) then
+            return
+        end
+
+        on_dir(vim.fs.root(bufnr, root_markers))
+    end
+end
+
+local function get_nvim_lua_root_dir(bufnr, on_dir)
+    if is_nvim_config_buffer(bufnr) then
+        on_dir(vim.fn.stdpath("config"))
+    end
+end
+
+local function get_nvim_lua_library()
+    local root = require("config.lazy").root
+
+    local library = { vim.env.VIMRUNTIME }
+
+    if vim.uv.fs_stat(root) then
+        vim.list_extend(library, vim.fn.globpath(root, "*", false, true))
+    end
+
+    return library
+end
+
 return {
     "neovim/nvim-lspconfig",
     dependencies = { "mason-org/mason.nvim" },
@@ -12,21 +49,38 @@ return {
         { "yoJ", vim.cmd.LspRestart },
     },
     config = function()
+        local lua_ls = vim.lsp.config.lua_ls
+        assert(lua_ls, "lua_ls config is not available")
+
         local opts = {
             lua_ls = {
+                root_dir = lua_root_dir_getter(lua_ls.root_markers),
                 settings = {
                     Lua = {
                         runtime = {
                             version = "LuaJIT",
                         },
                         workspace = {
-                            checkThirdParty = true,
                             library = { vim.env.VIMRUNTIME },
-                            -- library = vim.api.nvim_get_runtime_file("", true),
                         },
                     },
                 },
             },
+            lua_ls_nvim = vim.tbl_deep_extend("force", vim.deepcopy(lua_ls), {
+                root_markers = nil,
+                root_dir = get_nvim_lua_root_dir,
+                settings = {
+                    Lua = {
+                        runtime = {
+                            version = "LuaJIT",
+                        },
+                        workspace = {
+                            checkThirdParty = "Disable",
+                            library = get_nvim_lua_library(),
+                        },
+                    },
+                },
+            }),
             vimls = {},
             jsonls = {},
             taplo = {},
