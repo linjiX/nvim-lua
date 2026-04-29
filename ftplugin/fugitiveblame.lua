e683281ee76b8a271044785410db12f6f7074f7a
linji.xue <linji.xue@graviti.com>
2026-04-29 22:10:52 +0800

    fix(fugitive): fix the "reblame" feature will be blocked by "winfixbuf"

--- ftplugin/fugitiveblame.lua
+++ ftplugin/fugitiveblame.lua
@@ -24,3 +24,56 @@ vim.keymap.set("n", "<CR>", function()
         error(result)
     end
 end, { buffer = true })
+
+local function find_blame_source_win()
+    local current = vim.api.nvim_get_current_win()
+
+    for _, win in ipairs(vim.api.nvim_tabpage_list_wins(0)) do
+        if win ~= current and vim.api.nvim_win_is_valid(win) and vim.wo[win].scrollbind then
+            return win
+        end
+    end
+end
+
+local function reblame(suffix)
+    local win = find_blame_source_win()
+    local BlameJump = utility.get_script_function("BlameJump", scriptname)
+
+    vim.wo[win].winfixbuf = false
+
+    vim.cmd(BlameJump(suffix))
+
+    vim.wo[win].winfixbuf = true
+    vim.wo[win].cursorbind = true
+end
+
+for _, lhs in ipairs({ "-", "s", "u" }) do
+    vim.keymap.set("n", lhs, function()
+        reblame("")
+    end, {
+        buffer = true,
+        nowait = true,
+        desc = "Reblame at commit",
+    })
+end
+
+vim.keymap.set("n", "P", function()
+    if vim.v.count == 0 then
+        vim.api.nvim_echo({ { "Use ~ (or provide a count)", "ErrorMsg" } }, true, {})
+        return
+    end
+
+    reblame(("^%d"):format(vim.v.count1))
+end, {
+    buffer = true,
+    nowait = true,
+    desc = "Reblame at commit parent",
+})
+
+vim.keymap.set("n", "~", function()
+    reblame(("~%d"):format(vim.v.count1))
+end, {
+    buffer = true,
+    nowait = true,
+    desc = "Reblame at commit ancestor",
+})
