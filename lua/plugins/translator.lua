4ac7b129d71f8979444670879a1cc8c0740c365d
linji.xue <linji.xue@graviti.com>
2026-05-13 15:56:24 +0800

    fix(translator): fix the double border issue in translator float window

--- lua/plugins/translator.lua
+++ lua/plugins/translator.lua
@@ -16,6 +16,21 @@ return {
         vim.api.nvim_set_hl(0, "Translator", { link = "NormalFloat", default = true })
         vim.api.nvim_set_hl(0, "TranslatorBorder", { link = "FloatBorder", default = true })
 
+        vim.api.nvim_create_autocmd("FileType", {
+            group = vim.api.nvim_create_augroup("MyTranslator", { clear = true }),
+            pattern = { "translator", "translatorborder" },
+            callback = function(opts)
+                vim.schedule(function()
+                    for _, win in ipairs(vim.fn.win_findbuf(opts.buf)) do
+                        local config = vim.api.nvim_win_get_config(win)
+                        if config.relative ~= "" then
+                            vim.api.nvim_win_set_config(win, { border = "none" })
+                        end
+                    end
+                end)
+            end,
+        })
+
         vim.keymap.set("n", "<C-u>", function()
             if vim.fn["translator#window#float#has_scroll"]() ~= 0 then
                 return vim.fn["translator#window#float#scroll"](1)
