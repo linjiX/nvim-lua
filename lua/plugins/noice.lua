52cbc96b5e41fc628f399df01d09bf72098a8689
linji.xue <linji.xue@graviti.com>
2026-08-16 16:05:31 +0800

    feat(noice): set "long_message_to_split" to false
    Make sure git messages will be shown in floating window

--- lua/plugins/noice.lua
+++ lua/plugins/noice.lua
@@ -19,7 +19,7 @@ return {
             presets = {
                 bottom_search = false,
                 command_palette = true,
-                long_message_to_split = true,
+                long_message_to_split = false,
                 lsp_doc_border = true,
             },
             messages = {
