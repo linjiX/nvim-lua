3d32a7518c5439dd6315cd5ffc3708b224ebaa04
linji.xue <linji.xue@graviti.com>
2026-09-05 18:56:19 +0800

    build(telescope): use latest "telescope" version instead of 0.1.x branch

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -41,7 +41,6 @@ end
 
 return {
     "nvim-telescope/telescope.nvim",
-    branch = "0.1.x",
     dependencies = {
         "nvim-lua/plenary.nvim",
         { "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
